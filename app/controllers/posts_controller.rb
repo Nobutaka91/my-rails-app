7abd4cdf1d57ef4386addb7c982f56733eb0ff3c
Nobu <69408967+Nobutaka91@users.noreply.github.com>
2024-07-05 02:17:15 +0000

    CROUDを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,42 @@ class PostsController < ApplicationController
   def show # 個々の投稿を表示するためのアクション
     @post = Post.find(params[:id])
   end
+
+  def new 
+    @post = Post.new 
+  end
+
+  def create
+    @post = Post.new(post_params) # formのtitleとbodyに対して入力された値を取得している
+    if @post.save # saveの返り値をvalidationチェックに使用する
+      redirect_to root_path # データが保存された後に自動でリダイレクトするようにする
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end 
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else 
+      render :edit, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord # form部分のvalidatesの設定
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 5}
 end
